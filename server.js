cea6c919f40a76f75c0eb0836087a2c3ec931224
Tenlive1 <nguyenperry99@gmail.com>
2024-08-20 00:20:00 -0500

    done

--- server.js
+++ server.js
@@ -1,7 +1,38 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req,res) => {
-    console.log("I am in the server constant");
+    res.setHeader('Content-Type', 'text/html');
+
+    let path =  './view/';
+
+    switch(req.url){
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about':
+            path += 'about.html';
+            break;
+        case '/about-me':
+            res.setHeader('Location', '/about');
+            res.statusCode = 301;
+            res.end();
+        default:
+            path += '404.html';
+            break;
+        
+    }
+
+    fs.readFile(path, (err,data)=>{
+        if(err){
+            console.log(err);
+            res.end();
+        }else{
+            res.write(data);
+            res.end();
+        }
+    });
+
 });
 
 server.listen(3000, 'localhost' , () =>{
