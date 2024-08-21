7728efd9cc1d7d44abb5e59cd47336df4ccfd123
Tenlive1 <nguyenperry99@gmail.com>
2024-08-21 00:54:39 -0500

    done with episode 8

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // this will go from top to bottom in express
 
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
@@ -9,6 +10,9 @@ app.set('view engine', 'ejs');
 //listen for request
 
 app.listen(3000);
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
 
 app.get('/',(req,res) =>{
     const blogs = [
